c8a1852e167f81f4e29081ac9d999ae68baeba88
Jingyi Chen <chen.jingyi6@northeastern.edu>
2023-10-24 05:28:11 -0700

    update

--- kanbas-react-web-app-cs5610-fa23/src/index.js
+++ kanbas-react-web-app-cs5610-fa23/src/index.js
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App.js";
-// import reportWebVitals from "./reportWebVitals";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// // import "../public/vendors/css/bootstrap.min.css"
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -38,4 +18,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> d49b176fe87e5f34581f00ed671c73ba5863a562
